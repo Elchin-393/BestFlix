cebd9232b385cd2cb6de2ee4bb26b444c75ebb47
Elchin-393 <143759042+Elchin-393@users.noreply.github.com>
2025-08-09 15:07:23 +0400

    Delete backend directory

--- backend/src/main/java/com/bestflix/movie/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.bestflix.movie.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class UserNotFoundException extends BaseException{
-    public UserNotFoundException(String username){
-        super(new ErrorMessage(username, MessageType.NO_RECORD_EXIST) , HttpStatus.NOT_FOUND);
-    }
-}
